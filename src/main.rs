274c514af8a65e2517b6ba86aeed26369b75c385
ardeshir sepahsalar <ardeshir.org@gmail.com>
2020-03-08 15:19:20 -0500

    adding ops

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 use std::mem;
 
-fn main() {
-    println!("Building binary data app:");
+fn bitops() {
+
+ println!("\nDoing bitwise operations\n");
+
+ let c = 1 | 2 ; // | OR & AND ^ XOR ! NOR
+                 // 01 OR 10 = 11 == 3_10
+ println!("c: 1 | 2 = {} , size of c = {} bytes", c, mem::size_of_val(&c));
+ 
+ let two_to_10 = 1 << 10;  // >> 
+ println!("2^10 = {}", two_to_10);
+ 
+ // logical 
+ let pi_less_4 = std::f64::consts::PI < 4.0; // true
+ // > <= >=
+ println!("Pi < 4 = {}", pi_less_4);
+ let x = 5;
+ let x_is_5 = x == 5; // true 
+
+ println!(" x = {} x == 5: {} ", x, x_is_5); 
+
+}
+
+fn data_types() {
+    println!("\nBuilding binary data app:");
     let mut c = 123456789; // 32-bit signed i32
     println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
     c = -1;
@@ -14,6 +36,38 @@ fn main() {
     let d:char = 'd';
     // let x = 'x';
     println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
-    let e = 2.5;
+    let e:f32 = 2.5;
     println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+
+    let f = false;
+    println!("f = {}, size = {} bytes", f, mem::size_of_val(&f));
+
+}
+
+fn ops() {
+
+  // arithmetic
+  println!("\nDoing Arithmetic: \n");
+  let mut a = 2+3*4;
+  println!("a = {}, size = {} bytes", a, mem::size_of_val(&a));
+  a = a+1;  // rust doesn't support ++ or --
+  a -= 2; // a = a - 2
+          // -= += *= /=   %=  
+ println!("a = {}, size = {} bytes", a, mem::size_of_val(&a));
+ println!("Remainder of {} / {} = {}", a, 3, (a%3));
+ let a_cubed = i32::pow(a,3);
+ println!("{} cubed = {}, size = {} bytes", a, a_cubed, mem::size_of_val(&a));
+
+ let b = 2.5;
+ let b_cubed = f64::powi(b, 3);
+ let b_to_pi = f64::powf(b, std::f64::consts::PI);
+ println!("b = {}, cubed = {} {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+}
+
+fn main() {
+
+  data_types();
+  ops();
+  bitops();
 }
